39709855046eac704e77c590254397974881ac0b
Evandro Mateus <evandromateus06@gmail.com>
2021-07-30 22:20:14 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -41,4 +41,10 @@ describe('LoadUserByEmail Repository', () => {
     const user = await sut.load('email@email.com')
     expect(user._id).toEqual(fakeUser.insertedId)
   })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('invalid_email@email.com')
+    expect(promise).rejects.toThrow()
+  })
 })
