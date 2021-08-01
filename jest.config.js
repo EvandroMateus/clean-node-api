b3240e503d96adeef7f848a55a6813adb29efe90
Evandro Mateus <evandromateus06@gmail.com>
2021-07-31 21:26:38 -0300

    ensure UpdateAccessTokenRepository updates userwith the given accessToken

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['**/src/**/*.js'],
   preset: '@shelf/jest-mongodb',
-  watchPathIgnorePatterns: ['globalConfig']
+  watchPathIgnorePatterns: ['globalConfig.json']
 }
